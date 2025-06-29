f1516e96112d01c60b382e61f71dfaf7eac2ea48
ztzyrant <ztzyrant@gmail.com>
2025-06-30 02:19:20 +0800

    feat: use environment port or default to 3000
    - Modified the port that the Elysia app listens on to use the environment variable PORT if it is set, otherwise default to port 3000.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia").listen(process.env.PORT ?? 3000);
 
 app.get("/health", () => "OK");
 
